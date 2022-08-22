9271a96d28f12a49e31d3b430ba3334c37658d5c
vaux <zero.griffin@gmail.com>
2022-08-22 17:53:09 +0200

    Update share

--- macOS/Share.swift
+++ macOS/Share.swift
@@ -7,10 +7,12 @@ final class Share: NSView {
     private var subs = Set<AnyCancellable>()
     private let syncher: Syncher
     private let error = PassthroughSubject<String, Never>()
+    private let session: Session
     
     required init?(coder: NSCoder) { nil }
     init(session: Session, header: Header) {
-        self.syncher = .init(header: header)
+        self.session = session
+        syncher = .init(header: header)
         
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
@@ -67,7 +69,7 @@ final class Share: NSView {
         
         tryAgain
             .click
-            .sink {
+            .sink { [weak self] in
                 title.isHidden = false
                 warning.isHidden = true
                 fail.stringValue = ""
@@ -75,6 +77,10 @@ final class Share: NSView {
                 please.isHidden = false
                 tryAgain.state = .hidden
                 cancel.state = .hidden
+                
+                Task { [weak self] in
+                    await self?.share()
+                }
             }
             .store(in: &subs)
         
@@ -110,8 +116,6 @@ final class Share: NSView {
             }
             .store(in: &subs)
         
-        error.send("completion: hello world")
-        
         Task {
             try? await Task.sleep(nanoseconds: 450_000_000)
             session.selected.value = nil
@@ -135,122 +139,30 @@ final class Share: NSView {
         }
         
         do {
-//            let raw = try syncher.share()
-//
-//            let watermark = UIImage(named: "Watermark")!.cgImage!
-//            
-//            UIGraphicsBeginImageContext(.init(width: raw.width, height: raw.height))
-//            UIGraphicsGetCurrentContext()!.translateBy(x: 0, y: .init(raw.height))
-//            UIGraphicsGetCurrentContext()!.scaleBy(x: 1, y: -1)
-//            UIGraphicsGetCurrentContext()!.draw(raw,
-//                                                in: .init(origin: .zero,
-//                                                          size: .init(width: raw.width,
-//                                                                      height: raw.height)))
-//            UIGraphicsGetCurrentContext()!.draw(watermark,
-//                                                in: .init(origin: .init(x: (raw.width - size) / 2,
-//                                                                        y: (raw.height - size) / 2),
-//                                                          size: .init(width: size, height: size)))
-//            let image = UIImage(cgImage: UIGraphicsGetCurrentContext()!.makeImage()!)
-//            
-//            UIGraphicsEndImageContext()
-//            
-//            UIApplication.shared.isIdleTimerDisabled = false
-//            withAnimation(.easeOut(duration: 0.4)) {
-//                session.flow = .shared(syncher.header, image)
-//            }
+            let raw = try syncher.share()
+            
+            let watermark = NSImage(named: "Watermark")!
+            let code = NSImage(cgImage: raw, size: .init(width: raw.width, height: raw.height))
+            
+            let image = NSImage(size: .init(width: raw.width, height: raw.height))
+            image.lockFocus()
+            
+            code.draw(in: .init(origin: .zero, size: code.size))
+            watermark.draw(in: .init(origin: .init(x: (raw.width - syncher.size) / 2,
+                                                   y: (raw.height - syncher.size) / 2),
+                                     size: .init(width: syncher.size,
+                                                 height: syncher.size)))
+            
+            image.unlockFocus()
+             
+            let data = NSBitmapImageRep(cgImage: image.cgImage(forProposedRect: nil, context: nil, hints: nil)!)
+                 .representation(using: .png, properties: [:])!
+             
+            let result = NSImage(data: data)!
+            
+            session.flow.value = .shared(syncher.header, result)
         } catch {
             self.error.send(error.localizedDescription)
         }
     }
 }
-/*
-struct Share: View {
-    let session: Session
-    let syncher: Syncher
-    @State private var error: Error?
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            
-            Image(systemName: "square.and.arrow.up")
-                .font(.system(size: 60, weight: .ultraLight))
-                .symbolRenderingMode(.hierarchical)
-                .foregroundStyle(.primary)
-            
-            if error == nil {
-                Text("Sharing")
-                    .font(.title2.weight(.regular))
-                    .padding(.top)
-            }
-            
-            Text(syncher.header.title)
-                .font(.callout)
-                .foregroundStyle(.secondary)
-                .lineLimit(1)
-                .frame(maxWidth: 280)
-            
-            if let error = error {
-                Image(systemName: "exclamationmark.triangle")
-                    .font(.system(size: 50, weight: .ultraLight))
-                    .symbolRenderingMode(.hierarchical)
-                    .padding(.vertical)
-                Text(error.localizedDescription)
-                    .font(.callout)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.secondary)
-                    .frame(maxWidth: 280)
-                    .fixedSize(horizontal: false, vertical: true)
-            } else {
-                Text("Please wait")
-                    .font(.footnote)
-                    .foregroundStyle(.secondary)
-            }
-            
-            Spacer()
-            
-            if error != nil {
-                Button {
-                    error = nil
-                    
-                    Task {
-                        await share()
-                    }
-                } label: {
-                    Text("Try again")
-                        .font(.body.weight(.bold))
-                        .foregroundColor(.primary)
-                        .padding()
-                        .contentShape(Rectangle())
-                }
-                .padding(.bottom)
-                
-                Button(role: .destructive) {
-                    UIApplication.shared.isIdleTimerDisabled = false
-                    
-                    withAnimation(.easeIn(duration: 0.4)) {
-                        session.flow = .main
-                    }
-                } label: {
-                    Text("Cancel")
-                        .font(.callout.weight(.medium))
-                        .foregroundColor(.secondary)
-                        .padding()
-                        .contentShape(Rectangle())
-                }
-                .padding(.bottom, 30)
-            }
-        }
-        .task {
-            UIApplication.shared.isIdleTimerDisabled = true
-            
-            try? await Task.sleep(nanoseconds: 450_000_000)
-            session.selected = nil
-            
-            await share()
-        }
-    }
-    
-    
-}
-*/
