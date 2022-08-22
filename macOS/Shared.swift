7bdc88814123914f19f4108a06a983568e594c0a
vaux <zero.griffin@gmail.com>
2022-08-22 18:17:55 +0200

    Add shared

---
+++ macOS/Shared.swift
@@ -0,0 +1,78 @@
+import AppKit
+import Coffee
+import Offline
+
+final class Shared: NSView {
+    
+}
+/*
+struct Shared: View {
+    let session: Session
+    let header: Header
+    let image: UIImage
+    @State private var url: URL?
+    
+    var body: some View {
+        VStack {
+            Text(header.title)
+                .font(.title3.weight(.medium))
+                .lineLimit(1)
+                .frame(maxWidth: 280)
+                .padding(.top)
+            
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxHeight: 400)
+                .padding(.horizontal)
+            
+            Spacer()
+            
+            Button {
+                guard
+                    let url = url,
+                    let data = image.pngData()
+                else { return }
+                
+                try? data.write(to: url)
+                UIApplication.shared.share(url)
+            } label: {
+                Image(systemName: "square.and.arrow.up.circle")
+                    .font(.system(size: 45, weight: .thin))
+                    .symbolRenderingMode(.hierarchical)
+                    .foregroundColor(.primary)
+                    .padding()
+                    .contentShape(Rectangle())
+            }
+            
+            Spacer()
+            
+            Button {
+                if let url = url {
+                    try? FileManager.default.removeItem(at: url)
+                }
+                
+                withAnimation(.easeOut(duration: 0.3)) {
+                    session.flow = .main
+                }
+            } label: {
+                Text("Continue")
+                    .font(.title3.weight(.bold))
+                    .foregroundColor(.primary)
+                    .padding()
+                    .contentShape(Rectangle())
+            }
+            
+            Spacer()
+        }
+        .onAppear {
+            if Defaults.rate {
+                UIApplication.shared.review()
+            }
+        }
+        .task {
+            url = .init(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(header.title + ".png")
+        }
+    }
+}
+*/
